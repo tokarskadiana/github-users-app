fd3e668fcfb68c9da8ac2253e87337399a2a4219
dianatokarska <tokarskadiana@gmail.com>
2020-06-20 01:35:08 +0200

    Add router

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,18 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 import styles from "./styles/index.css";
 import Users from "./components/Users/Users";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => (
   <Provider store={store}>
     <div className={styles.App}>
-      <Users />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Users />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   </Provider>
 );
