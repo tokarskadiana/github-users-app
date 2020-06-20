9b54c1a91cfb4207330027d98bf3f33e440ee9b1
dianatokarska <tokarskadiana@gmail.com>
2020-06-20 04:07:17 +0200

    Add scss support

--- typings/modules.d.ts
+++ typings/modules.d.ts
@@ -7,3 +7,8 @@ declare module '*.css' {
   const content: {[className: string]: string};
   export default content;
 }
+
+declare module '*.scss' {
+  const content: {[className: string]: string};
+  export default content;
+}
