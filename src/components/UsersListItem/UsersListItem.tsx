900911abd4a76cd82fae824e2619253ccbb1814d
dianatokarska <tokarskadiana@gmail.com>
2020-06-20 02:15:48 +0200

    Redirect to user details on users list item click

--- src/components/UsersListItem/UsersListItem.tsx
+++ src/components/UsersListItem/UsersListItem.tsx
@@ -3,6 +3,7 @@ import styles from "./UsersListItem.css";
 import { connect } from "react-redux";
 import { setSelectedUser } from "../../actions";
 import { Action } from "redux";
+import { Link } from "react-router-dom";
 
 type Props = {
   user: User;
@@ -18,12 +19,16 @@ const UsersListItem = ({ user, setSelectedUser }: Props) => {
   const handleClick = () => setSelectedUser(user);
 
   return (
-    <div className={styles.user} onClick={handleClick}>
+    <Link
+      to={`/users/${user.username}`}
+      className={styles.user}
+      onClick={handleClick}
+    >
       <div className={styles.avatarWrapper}>
         <img src={user.avatarUrl} alt="avatar" style={avatarStyles} />
       </div>
       <span className={styles.username}>@{user.username}</span>
-    </div>
+    </Link>
   );
 };
 
